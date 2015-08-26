fee5ac5e2204d1411732d3d5fb777651d6bbaad1
Kaveh <kaveh256@gmail.com>
2015-08-25 23:33:54 -0400

    Adding an improved version of merge sort.

---
+++ python/ch02/merge_sort_improved_test.py
@@ -0,0 +1,17 @@
+from merge_sort_improved import merge_sort
+
+import unittest
+
+
+class MergeSortTests(unittest.TestCase):
+    def test_reverse_order(self):
+        self.assertEqual(merge_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]),
+                         [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
