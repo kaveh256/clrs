e79678a8c599be4c2466a08d63fd6fd429ff5e2b
Kaveh <kaveh256@gmail.com>
2015-08-25 20:36:58 -0400

    Added insertion sort

---
+++ python/ch02/insertion_sort.py
@@ -0,0 +1,24 @@
+def insertion_sort(items):
+    """Sorts a list of items.
+
+    Uses inserstion sort to sort the collection items.
+    
+    Args:
+        items: A list of items.
+
+    Returns:
+        The sorted list of items. 
+    """
+
+    num = len(items)
+    for j in range(1, num):
+        key = items[j]
+
+        # Insert items[j] into the sorted sequence items[1..j-1]
+        i = j - 1
+        while i >= 0 and items[i] > key:
+            items[i + 1] = items[i]
+            i = i - 1
+        items[i + 1] = key
+
+    return items
